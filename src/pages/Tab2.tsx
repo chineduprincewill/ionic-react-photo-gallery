6b32973589d982785f61148d6522061b9714dc0b
Chinedu Ijeomah <cijeomah@apin.local>
2024-04-15 11:34:13 +0100

    added delete photo feature

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,12 +1,15 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg, IonActionSheet } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
-import { camera } from 'ionicons/icons';
-import { usePhotoGallery } from '../hooks/usePhotoGallery';
+import { camera, trash, close } from 'ionicons/icons';
+import { UserPhoto, usePhotoGallery } from '../hooks/usePhotoGallery';
+import { useState } from 'react';
 
 const Tab2: React.FC = () => {
 
-  const { photos, takePhoto } = usePhotoGallery();
+  const { photos, takePhoto, deletePhoto } = usePhotoGallery();
+
+  const [photoToDelete, setPhotoToDelete] = useState<UserPhoto>();
 
   return (
     <IonPage>
@@ -15,12 +18,34 @@ const Tab2: React.FC = () => {
           <IonTitle>Photo Gallery</IonTitle>
         </IonToolbar>
       </IonHeader>
+      <IonActionSheet 
+        isOpen={!!photoToDelete}
+        buttons={[
+          {
+            text: 'Delete',
+            role: 'destructive',
+            icon: trash,
+            handler: () => {
+              if(photoToDelete) {
+                deletePhoto(photoToDelete);
+                setPhotoToDelete(undefined);
+              }
+            },
+          },
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            icon: close,
+          },
+        ]}
+        onDidDismiss={() => setPhotoToDelete(undefined)}
+      />
       <IonContent fullscreen>
         <IonGrid>
           <IonRow>
             {photos.map((photo, index) => (
               <IonCol size='6' key={photo.filepath}>
-                <IonImg src={photo.webviewPath} />
+                <IonImg onClick={() => setPhotoToDelete(photo)} src={photo.webviewPath} />
               </IonCol>
             ))}
           </IonRow>
